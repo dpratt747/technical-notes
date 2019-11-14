858f6eb20851f7af42cc00ab7c2b7c29a6fcae9d
Pratt <david.pratt@ynap.com>
2019-11-14 17:15:04 +0000

    introduction of config values in dockerpostgresservice used for tests

--- src/test/scala/test_utils/DockerPostgresService.scala
+++ src/test/scala/test_utils/DockerPostgresService.scala
@@ -3,8 +3,9 @@ package test_utils
 import java.sql.DriverManager
 
 import com.whisk.docker.{DockerCommandExecutor, DockerContainer, DockerContainerState, DockerKit, DockerReadyChecker}
+import io.github.dpratt747.technical_notes.domain.adt.configuration.{Conf, DatabaseName, HostName, Password, Port, UserName}
+import pureconfig.ConfigSource
 import pureconfig.generic.auto._
-
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
@@ -12,24 +13,29 @@ import scala.util.Try
 
 protected trait DockerPostgresService extends DockerKit {
 
-  private val r = scala.util.Random
-  private val postgresUser = "postgres"
-  private val postgresPassword = "docker"
+  private val config = ConfigSource.default.loadOrThrow[Conf]
+  val postgresPort: Port = config.postgres.port
+  val databaseName: DatabaseName = config.postgres.properties.databaseName
+  val postgresUser: UserName = config.postgres.properties.user
+  val postgresPassword: Password = config.postgres.properties.password
+  val postgresHostName: HostName = config.postgres.hostName
+
+  private val random = scala.util.Random
 
   // max port number ranges between 49152 and 65535; opting to use lower bound
-  private val upperBound = 49152 - 80
-  private lazy val randomPort = 80 + r.nextInt(upperBound)
+  private val upperBound = 49152 //todo: may not need to add or minus 80
+  private lazy val randomPort = random.nextInt(upperBound)
+
 
   val postgresContainer: DockerContainer = {
     DockerContainer("postgres:latest")
       .withPorts((postgresAdvertisedPort, Some(postgresExposedPort)))
-      .withEnv(s"POSTGRES_USER=$postgresUser", s"POSTGRES_PASSWORD=$postgresPassword", "POSTGRES_DB=technical_notes_test")
+      .withEnv(s"POSTGRES_USER=${postgresUser.value}", s"POSTGRES_PASSWORD=${postgresPassword.value}", s"POSTGRES_DB=${databaseName.value}")
       .withReadyChecker(
-    new PostgresReadyChecker(postgresUser, postgresPassword, Some(postgresExposedPort))
+    new PostgresReadyChecker(postgresUser.value, postgresPassword.value, Some(postgresExposedPort))
       .looped(15, 1.second)
     )
   }
-
   final def postgresAdvertisedPort: Int = 5432
 
   final def postgresExposedPort: Int = randomPort
